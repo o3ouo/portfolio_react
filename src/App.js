4bb09b3a84a7bb98b2a6e35280f9db7fddd86f95
o3ouo <o3ouo@daum.net>
2025-03-10 16:51:15 +0900

    design end 250310

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+function setFullScreenHeight() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+if (window.innerWidth > window.innerHeight && window.innerHeight < 480) {
+  document.querySelector(".fullscreen").style.minHeight = "480px";
+  document.querySelector(".fullscreen").style.overflowY = "auto";
+} else {
+  document.querySelector(".fullscreen").style.minHeight = "";
+  document.querySelector(".fullscreen").style.overflowY = "";
+}
+
+window.addEventListener("resize", setFullScreenHeight);
+setFullScreenHeight();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App fullscreen">
+   
     </div>
   );
 }
