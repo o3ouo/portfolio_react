adacd615d2859b5a5d2519de595e34faf4518b7b
o3ouo <o3ouo@daum.net>
2025-03-17 15:50:07 +0900

    notion gogo

--- src/component/LinkAppList.js
+++ src/component/LinkAppList.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import WorkContent from './WorkContent';
 
+// 노션 메뉴
+const NotionMenu = ({ quichMenu, onClose }) => {
+  if (!quichMenu) return null;
+
+  return (
+    <div className="notion_overlay" onClick={onClose}>
+      <motion.div
+        className="notion_content"
+        initial={{ y: "100%", opacity: 1 }}
+        anumate={{ y: 0, opacity: 1 }}
+        exit={{ y: "100%", opacity: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="notion_inner">
+
+        </div>
+      </motion.div>
+    </div>
+  )
+}
+
 function LinkAppList() {
+  const [notionClick, setNotionClick] = useState(false);
+
+
   return (
     <div className="link_app_list">
-      <WorkContent title={"Notion"} icon={"notion"}/>
-      <WorkContent title={"Naver"} icon={"naver"} />
-      <WorkContent title={"Daum"} icon={"daum"}/>
-      <WorkContent title={"TodoList"} icon={"todolist"}/>
+      <WorkContent title={"Notion"} icon={"notion"} onClick={() => setNotionClick()}  />
+
+      <Link to="https://www.naver.com/" target="_blank">
+        <WorkContent title={"Naver"} icon={"naver"}/>
+      </Link>
+      
+      <Link to="https://www.daum.net/" target="_blank">
+        <WorkContent title={"Daum"} icon={"daum"}/>
+      </Link>
+
+      <Link to="https://github.com/o3ouo?tab=repositories" target="_blank">
+        <WorkContent title={"GitHub"} icon={"github"}/>
+      </Link>
     </div>
   );
 }
--- src/component/WorkList.js
+++ src/component/WorkList.js
@@ -7,16 +7,20 @@ function WorkList() {
 
   return (
     <div className="work_list">
-      <WorkContent 
-        title={"Web Redesign"} 
-        icon={"web_redesign"} 
+      <WorkContent
+        title={"Web Redesign"}
+        icon={"web_redesign"}
         onClick={() => navigate("/web_redesign")}
       />
-      <WorkContent title={"Clone Codding"} icon={"phils"} />
-      <Link to="https://o3ouo.github.io/news_weather/" target="_blank">
-        <WorkContent title={"Play Press"} icon={"play_press"} />    
+      <Link to="https://o3ouo.github.io/phils/" target="_blank">
+        <WorkContent title={"Clone Codding"} icon={"phils"} />
       </Link>
-      <WorkContent title={"Portfolio 2024"} icon={"portfolio"} />
+      <Link to="https://o3ouo.github.io/news_weather/" target="_blank">
+        <WorkContent title={"Play Press"} icon={"play_press"} />
+      </Link>
+      <Link to="https://o3ouo.github.io/portfolio_vjs/" target='blank'>
+        <WorkContent title={"Portfolio 2024"} icon={"portfolio"} />
+      </Link>
     </div>
   );
 }
